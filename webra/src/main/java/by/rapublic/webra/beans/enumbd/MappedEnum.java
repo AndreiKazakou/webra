1cacafb4927193a783b7cd9489d03991377468d9
Andrei_Kazakou <Andrei_Kazakou@EPBYGOMW0292.gomel.epam.com>
2013-10-02 18:54:32 +0300

    new

--- webra/src/main/java/by/rapublic/webra/beans/enumbd/MappedEnum.java
+++
@@ -1,9 +0,0 @@
-package by.rapublic.webra.beans.enumbd;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(value = RetentionPolicy.RUNTIME)
-public @interface MappedEnum {
-	Class<? extends Enum> enumClass();
-}
--- webra/src/main/java/by/rapublic/webra/beans/enumbd/enuminterface/SessionAction.java
+++
@@ -1,7 +0,0 @@
-package by.rapublic.webra.beans.enumbd.enuminterface;
-
-import org.hibernate.Session;
-
-public interface SessionAction {
-	void run(Session session) throws Exception;
-}
